3d51eca733dd42bb14f8dc851da7b8d7f8e36b19
lcmiles <lcmiles@uab.edu>
2023-09-19 11:39:25 -0500

    Committing changes

--- src/HW2.java
+++ src/HW2.java
@@ -10,7 +10,7 @@ public class HW2 {
     private static final int insertionSortThreshold = 7; //sets the constant insertion sorting threshold for the modified merge sort algorithm
 
     public static void main(String[] arg) {
-        try (Scanner reader = new Scanner(Paths.get("1000.txt"))) {
+        try (Scanner reader = new Scanner(Paths.get("1000000.txt"))) {
             while (reader.hasNextLine()) {
             String intString = reader.nextLine();
             String[] stringArray = intString.split("\\s*,\\s*"); //splits the string at each " , " and adds each individual string to an array
